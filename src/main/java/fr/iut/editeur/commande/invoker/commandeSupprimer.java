b08c87004a0bf4ccd59e07a20c5a6198dca4b561
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-11-08 09:40:21 +0100

    feat : ajout de la classe

--- src/main/java/fr/iut/editeur/commande/invoker/commandeSupprimer.java
+++ src/main/java/fr/iut/editeur/commande/invoker/commandeSupprimer.java
@@ -1,4 +1,23 @@
 package fr.iut.editeur.commande.invoker;
 
-public class commandeSupprimer {
+import fr.iut.editeur.commande.CommandeDocument;
+import fr.iut.editeur.document.Document;
+
+public class commandeSupprimer extends CommandeDocument {
+
+    public commandeSupprimer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : effacer;start;end");
+            return;
+        }
+
+        this.document.suppression(Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]));
+        super.executer();
+    }
+
+
 }
