c2ad19b29a9aa843037c8ddb30f697c7c9844ca4
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-11-08 08:51:19 +0100

    Implémentation de la classe CommandeRemplacer

---
+++ src/main/java/fr/iut/editeur/commande/invoker/CommandeRemplacer.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande.invoker;
+
+import fr.iut.editeur.commande.CommandeDocument;
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;start;end;texte");
+            return;
+        }
+        int start = Integer.parseInt(parameters[1]);
+        int end = Integer.parseInt(parameters[2]);
+        String remplacement = parameters[3];
+        this.document.remplacer(start, end, remplacement);
+        super.executer();
+    }
+
+}
