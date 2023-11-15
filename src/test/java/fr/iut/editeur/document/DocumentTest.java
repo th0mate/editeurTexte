be577216ccb17e60420136e7ce899579eabc584b
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2023-11-15 09:44:20 +0100

    Test : ajout d'un test

---
+++ src/test/java/fr/iut/editeur/document/DocumentTest.java
@@ -0,0 +1,23 @@
+package fr.iut.editeur.document;
+
+import org.junit.Test;
+
+public class DocumentTest {
+
+    @Test
+    public void testDocument() {
+        Document document = new Document();
+        //faire un test automatique qui passe ou ne passe pas
+        document.clear();
+        document.ajouter("Bonjour");
+        //on vérifie que le document contient bien "Bonjour"
+        if (!document.getTexte().equals("Bonjour")) {
+            throw new RuntimeException("Le document ne contient pas le texte attendu");
+        }
+    }
+
+
+
+
+
+}
